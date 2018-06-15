2cd37cdec90bdbbd6c60d7ce34d5aab502185b46
Max <maximilian.quinones@gmail.com>
2018-06-14 19:03:01 -0700

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("test"),
         ),
-        body: new Center(child: new Text("Center"),
+        body: new Center(child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
